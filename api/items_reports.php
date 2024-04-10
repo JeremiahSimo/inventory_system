bbb1f98eccf19006f9062271467c93f5f765ea76
JeremiahSimo <jeremiahsimo26@gmail.com>
2024-04-10 19:45:17 +0800

    updated database

--- api/items_reports.php
+++ api/items_reports.php
@@ -1,109 +1,108 @@
+<?php
+  include './include/connection.php';
+  include './class/edit.php';
+  include './class/delete.php';
 
-	  <?php
-				include './include/connection.php';
-        include './class/edit.php';
-        include './class/delete.php';
+  $item_name = '';
+  if(isset($_POST['btn_filter'])) {
+    // Set $item_name to the selected item from the dropdown
+    $item_name = isset($_POST['item_name_dropdown']) ? $_POST['item_name_dropdown'] : '';
+  }
+  
+  $item_name_dropdown_value = $mysqli->query("SELECT * FROM items_table") or die ($mysqli->error);
+  $result = null; // Initialize $result variable
+  
+  if (!empty($item_name)){
+    // If an item is selected, apply filtering
+    $query = "SELECT i.logs_timestamp_id,
+              i.logs_timestamp,
+              it.item_name,
+              i.item_serial_number,
+              ls.log_status_name,
+              i.item_quantity,
+              el.employee_lname,
+              el.employee_fname
+            FROM item_logs_timestamp i
+            INNER JOIN items_table it ON it.item_id=i.item_id
+            INNER JOIN employee_list el ON el.employee_id=i.employee_id
+            INNER JOIN logs_status ls ON ls.log_status_id=i.logs_status_id
+            WHERE i.logs_timestamp_id>=1 
+            AND i.item_id = '$item_name'
+            ORDER BY i.logs_timestamp DESC";
+    
+    $result = $mysqli->query($query) or die ($mysqli->error);
+  } else {
+    // If no item is selected, fetch all items
+    $query = "SELECT i.logs_timestamp_id,
+              i.logs_timestamp,
+              it.item_name,
+              i.item_serial_number,
+              ls.log_status_name,
+              i.item_quantity,
+              el.employee_lname,
+              el.employee_fname
+            FROM item_logs_timestamp i
+            INNER JOIN items_table it ON it.item_id=i.item_id
+            INNER JOIN employee_list el ON el.employee_id=i.employee_id
+            INNER JOIN logs_status ls ON ls.log_status_id=i.logs_status_id
+            WHERE i.logs_timestamp_id>=1 
+            ORDER BY i.logs_timestamp DESC";
+    
+    $result = $mysqli->query($query) or die ($mysqli->error);
+  }
+?>
 
-
-        $item_name = '';
-        if(isset($_POST['btn_filter'])) {
-          // Set $item_name to the selected item from the dropdown
-          $item_name = isset($_POST['item_name_dropdown']) ? $_POST['item_name_dropdown'] : '';
-      }
-     
-      $item_name = isset($_POST['item_name_dropdown']) ? $_POST['item_name_dropdown'] : '';
-			$query="SELECT i.logs_timestamp_id,
-      i.logs_timestamp,
-        it.item_name,
-        i.item_serial_number,
-        ls.log_status_name,
-        i.item_quantity,
-        el.employee_lname,
-        el.employee_fname
-    FROM item_logs_timestamp i
-    INNER JOIN items_table it ON it.item_id=i.item_id
-    INNER JOIN employee_list el on el.employee_id=i.employee_id
-    INNER JOIN logs_status ls ON ls.log_status_id=i.logs_status_id
-    WHERE i.logs_status_id=1 ";
-   
-
-    $item_name_dropdown_value=$mysqli->query("SELECT * FROM items_table") or die ($mysqli->error);
-   if (!empty($item_name)){
-    $query .= "AND i.item_id = '$item_name'";
-   }
-   $query .= "ORDER BY i.logs_timestamp DESC ";
-
-			//mo gana ang search pag 4 or multiple fields and iyang e search, below 4 fields dli mo ganah ang multiple field search
-      //ang pagination kay dli mo ganah basta subra ra ang special characters in a certain fields
-      $result=$mysqli->query($query) or die ($mysqli->error);
-      $item_result=$mysqli->query("SELECT item_id, item_name FROM items_table WHERE delete_status=0")
-		?>
-		<script>
-    // Set the value of the dropdown using PHP variable
-    document.getElementById('item_name_dropdown').value = '<?php echo $item_name; ?>';
+<script>
+  // Set the value of the dropdown using PHP variable
+  document.getElementById('item_name_dropdown').value = '<?php echo $item_name; ?>';
 </script>
 
+<h3><i class="fa fa-angle-right"></i> Items Receiving</h3>
 
-        <h3><i class="fa fa-angle-right"></i> Items Receiving</h3>
-       
 <form method="POST" action="index_admin.php?page=items_reports">
-                <label for="violationDropdown">Item name:</label>
-                <select style="width: 250px;height:30px;margin-right:10px;"  name="item_name_dropdown" id="item_name_dropdown">
-                    <option value="">ALL</option>
-                      <?php
-                        while ($row_itemName = $item_name_dropdown_value->fetch_assoc()):
-                            ?>
-                          <option value="<?php echo $row_itemName['item_id']; ?>"
-                            <?php echo ($row_itemName['item_name'] == $item_name) ? 'selected' : ''; ?>>
-                                <?php echo $row_itemName['item_name']; ?>
-                          </option>
-                       <?php endwhile; ?>
-                </select>
-                <input type="submit" value="Filter">
-</form>          
-                    
-                      
-		
+  <label for="violationDropdown">Item name:</label>
+  <select style="width: 250px;height:30px;margin-right:10px;"  name="item_name_dropdown" id="item_name_dropdown">
+    <option value="">ALL</option>
+    <?php while ($row_itemName = $item_name_dropdown_value->fetch_assoc()): ?>
+      <option value="<?php echo $row_itemName['item_id']; ?>" <?php echo ($row_itemName['item_id'] == $item_name) ? 'selected' : ''; ?>>
+        <?php echo $row_itemName['item_name']; ?>
+      </option>
+    <?php endwhile; ?>
+  </select>
+  <input type="submit" name="btn_filter" value="Filter">
+</form>
 
- <table id="hidden-table-info" class="table datatable" name="btn_filter">
-                <thead>
-                  <tr>
-                    <th>Log ID</th>
-                    <th>Item name</th>
-                    <th>Serial Number</th>
-                    <th>Total Quantity</th>
-                    <th>Log status</th>
-                    <th>Log Timestamp</th>
-                    <th>Received by</th>
-				        		
-                  </tr>
-                </thead>
-				
-                <tbody>
-                <?php
-					while ($row=$result->fetch_assoc()):	
-				?>
-                  <tr class="gradeA">
-                   <td><?php echo $row['logs_timestamp_id'];?></td>
-				          	<td><?php echo $row['item_name'];?></td>
-                    <td><?php echo $row['item_serial_number'];?></td>
-                    <td><?php echo $row['item_quantity'];?></td>
-                    <td><?php echo $row['log_status_name'];?></td>
-                    <td><?php echo $row['logs_timestamp'];?></td>
-                    <td><?php echo $row['employee_lname'].', '. $row['employee_fname'];?></td>
+<table id="hidden-table-info" class="table datatable" name="btn_filter">
+  <thead>
+    <tr>
+      <th>Log ID</th>
+      <th>Item name</th>
+      <th>Serial Number</th>
+      <th>Total Quantity</th>
+      <th>Log status</th>
+      <th>Log Timestamp</th>
+      <th>Received by</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while ($row = $result->fetch_assoc()): ?>
+      <tr class="gradeA">
+        <td><?php echo $row['logs_timestamp_id'];?></td>
+        <td><?php echo $row['item_name'];?></td>
+        <td><?php echo $row['item_serial_number'];?></td>
+        <td><?php echo $row['item_quantity'];?></td>
+        <td><?php echo $row['log_status_name'];?></td>
+        <td><?php echo $row['logs_timestamp'];?></td>
+        <td><?php echo $row['employee_lname'].', '. $row['employee_fname'];?></td>
+      </tr>
+    <?php endwhile; ?>
+  </tbody>
+</table>
 
-                  
-					
-                  </tr>
-                  <?php endwhile; ?>
-				  			  
-                </tbody>
-              </table>
-                    
-	<button onclick="printTable()">Print</button>
-  <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+<button onclick="printTable()">Print</button>
+<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 <script>
-  function printTable(){
+  function printTable() {
     // Create a new window or tab
     var printWindow = window.open('', '_blank');
     
@@ -127,6 +126,4 @@
     // Print the new window
     printWindow.print();
   }
-  
 </script>
-    
